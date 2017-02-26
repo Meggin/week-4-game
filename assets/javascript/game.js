6142574aa1c14bf62c65476719d78c67f369888e
mkearney@google.com <mkearney@google.com>
2017-02-25 16:26:58 -0800

    gotta submit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,24 @@
 $(document).ready(function() {
 
-			// Set global variables.
+	// Boolean true when character selected.
 	var characterSelected;
+
+	// Boolean true when enemy selected.
 	var enemySelected;
+
+	// Boolean true when enemy defeated.
 	var enemyDefeated;
+
+	// Boolean true when enemies still available.
 	var enemiesAvailable;
+
+	// Character attack power increases with each attack.
 	var powerSurge = 6;
-	var counter = 0;
+
+	// Boolean true when you can still attack enemies.
 	var gameOn = true;
 
-	// Create array of possible characters.
+	// Create array characters and enemies.
 	var characterList = [
 		{
 			name: "Obi-Wan Kenobi",
@@ -40,8 +49,17 @@ $(document).ready(function() {
 			cap: 20
 		}
 	]
-	
 
+	// Create initial list of available characters.
+	createCharacterList(characterList);
+
+	// Respond to any click on any character list item.
+	addLiClickListeners();
+
+	// Respond to any click on attack button.
+	addAttClickListeners();
+	
+	// Create and add characters and bios to DOM.
 	function createCharacterList(characterList) {
 		
 		// Creates DOM elements for available characters from characterList[];
@@ -83,10 +101,10 @@ $(document).ready(function() {
 		};
 	};
 
-	createCharacterList(characterList);
-
+	// Handle any clicks on a character.
 	function addLiClickListeners() {
-		// Event fires any time click on list item.
+
+		// Event fires any time click on character list item.
 		$("li").on("click", function() {
 
 			// Don't do anything if character and enemy selected.
@@ -96,32 +114,39 @@ $(document).ready(function() {
 			// Pick enemy only after character selection.
 			// But your character can't also be your enemy.
 			} else if (characterSelected && ($(this).attr("data-isCharacter") == "false")) {
-				console.log("You can pick the enemy.");
-				var selectedEnemy = $(this);
-				console.log("You are able to select your enemy: " + selectedEnemy.attr("data-character"));
-				selectedEnemy.attr("data-isEnemy", "true");
-				console.log("Is your enemy selected?" + selectedEnemy.attr("data-isEnemy"));
 
-				// Add selected enemy to DOM.
+				// Set enemy to selected list item.
+				var selectedEnemy = $(this);
+
+				// Set enemy attribute to true.
+				selectedEnemy.attr("data-isEnemy", "true");
+
+				// Add selected enemy to DOM in fight arena.
 				// Use prepend so next enemy is always first.
 				selectedEnemy.prependTo($("#selected-enemy"));
 
-				// Remove this class to track available characters.
-				selectedEnemy.removeClass("available-characters");
+				// Remove class so that enemy characters don't appear.
+				// Without this, they duplicate on game lost reset.
+				//selectedEnemy.removeClass("available-characters");
 
-				// Keep track of enemy selection.
+				// Track enemy selection.
 				enemySelected = true;
 
+				// Remove this class so that enemies don't appear in character list on reset.
 				$(".characterList").removeClass($("#available-characters"));
 
+				// Rename enemy class so that you can iterate through enemies later.
 				$("ol").addClass("enemyList").removeClass("characterList");
 
-			// Pick your character.
+
+			// Choose character.
 			} else {
+
+				// Selected character item is now character.
 				var selectedCharacter = $(this);
-				console.log("You are able to select your character: " + selectedCharacter.attr("data-character"));
+
+				// Set character attribute to true.
 				selectedCharacter.attr("data-isCharacter", "true");
-				console.log("Is your character selected?" + selectedCharacter.attr("data-isCharacter"));
 
 				// Add selected character to DOM.
 				selectedCharacter.appendTo($("#character-placeholder"));
@@ -132,117 +157,74 @@ $(document).ready(function() {
 				// Keep track of character selection.
 				characterSelected = true;
 
+				// Need to remove this class so reset is clean.
+				// The class is recreated on reset, and you don't want duplicates.
 				$(".characterList").removeClass($("#available-characters"));
 
+				// Create enemy list once character selected.
+				// Called at start and reset of game.
 				$("ol").addClass("enemyList").removeClass("characterList");
 
-				// Update list of available enemies in DOM.
+				// Append enemy list to DOM.
 				$(".enemyList").appendTo($("#available-enemies"));
 			}
 		});
 	}
-
-	addLiClickListeners();
 	
-	$("#attack").on("click", function() {
+	// Respond to attack clicks in many ways.
+	function addAttClickListeners() {
 
-		if (gameOn) {
+		// Event fires on attack button clicks.
+		$("#attack").on("click", function() {
 
-			var selectedCharacter = $("#selected-character #character-placeholder .ui-widget-content").html();
+			// Fighting can only continue when game is on.
+			if (gameOn) {
 
-			var selectedEnemy = $("#fight-section #selected-enemy .ui-widget-content .characterName").html();
+				// Access character and enemy information in the DOM>
+				var selectedCharacter = $("#selected-character #character-placeholder .ui-widget-content").html();
+				var selectedEnemy = $("#fight-section #selected-enemy .ui-widget-content .characterName").html();
+				var selectedCharacterHealthPoints = $("#selected-character #character-placeholder .ui-widget-content .characterHealth").html();
+				var selectedCharacterAttackPower = $("#selected-character #character-placeholder .ui-widget-content").attr("data-attackPower");
+				var selectedEnemyHealthPoints = $("#fight-section #selected-enemy .ui-widget-content .characterHealth").html();
+				var selectedEnemyCounterAttackPoints = $("#fight-section #selected-enemy .ui-widget-content").attr("data-counterAttackPower");
 
-			var selectedCharacterHealthPoints = $("#selected-character #character-placeholder .ui-widget-content .characterHealth").html();
-			console.log("Selected Character Health Points = " + selectedCharacterHealthPoints);
+				// Parse DOM strings into integers.
+				selectedCharacterHealthPoints = parseInt(selectedCharacterHealthPoints);
+				selectedCharacterAttackPower = parseInt(selectedCharacterAttackPower);
+				selectedEnemyHealthPoints = parseInt(selectedEnemyHealthPoints);
+				selectedEnemyCounterAttackPoints = parseInt(selectedEnemyCounterAttackPoints);
 
-			selectedCharacterHealthPoints = parseInt(selectedCharacterHealthPoints);
-			
-			var selectedCharacterAttackPower = $("#selected-character #character-placeholder .ui-widget-content").attr("data-attackPower");
-			console.log("Character Attack Power = " + selectedCharacterAttackPower);
+				// If character has more health than enemy attack, attack.
+				if ((selectedCharacterHealthPoints - selectedEnemyCounterAttackPoints) > 0) {
+					
+					// Attack and assess damage.
+					characterAttacks(selectedCharacterHealthPoints, selectedEnemyCounterAttackPoints, selectedCharacterAttackPower, selectedEnemy);
+		 		
+		 		// Your character has lost and cgame is over.
+		 		} else {
+		 			
+		 			// Process game over.
+		 			characterLost(selectedCharacterHealthPoints, selectedEnemyCounterAttackPoints);
+		 			gameOn = false;
+		 		}
 
-			selectedCharacterAttackPower = parseInt(selectedCharacterAttackPower);
-			
-			var selectedEnemyHealthPoints = $("#fight-section #selected-enemy .ui-widget-content .characterHealth").html();
-			console.log("Enemy Health Points = " + selectedEnemyHealthPoints);
+		 		// If enemy has more health points than character attack power, attack.
+				if ((selectedEnemyHealthPoints - selectedCharacterAttackPower) > 0) {
+					enemyAttacksBack(selectedEnemyHealthPoints, selectedCharacterAttackPower);
+		 		
+				// Enemy lost and you will pick new enemy.
+		 		} else if ((selectedEnemyHealthPoints - selectedCharacterAttackPower) <= 0) {
 
-			selectedEnemyHealthPoints = parseInt(selectedEnemyHealthPoints);
-			
-			var selectedEnemyCounterAttackPoints = $("#fight-section #selected-enemy .ui-widget-content").attr("data-counterAttackPower");
-			console.log("Enemy Counter Attack Points = " + selectedEnemyCounterAttackPoints);
-
-			selectedEnemyCounterAttackPoints = parseInt(selectedEnemyCounterAttackPoints);
-
-			if ((selectedCharacterHealthPoints - selectedEnemyCounterAttackPoints) > 0) {
-	 			selectedCharacterHealthPoints = selectedCharacterHealthPoints - selectedEnemyCounterAttackPoints;
-	 			$("#selected-character #character-placeholder .ui-widget-content .characterHealth").html(selectedCharacterHealthPoints);
-	 			var characterReport = "You attacked " + selectedEnemy + " for " + selectedCharacterAttackPower + " damage.";
-	 			var enemyReport = selectedEnemy + " attacked you back for " + selectedEnemyCounterAttackPoints + " damage.";
-	 			$("#attack-report").text(characterReport);
-	 			$("#losses-report").text(enemyReport);
-	 		} else {
-	 			var gameLostMessage = "You've been defeated . . . GAME OVER!!!";
-	 			selectedCharacterHealthPoints = selectedCharacterHealthPoints - selectedEnemyCounterAttackPoints;
-	 			$("#selected-character #character-placeholder .ui-widget-content .characterHealth").html(selectedCharacterHealthPoints);
-	 			$("#attack-report").text(gameLostMessage);
-	 			$("#losses-report").empty();
-	 			var restartBtn = $("<button>");
-	 			restartBtn.text("Restart");
-	 			restartBtn.addClass("restart");
-	 			restartBtn.appendTo("#fight-section");
-	 			gameOn = false;
-	 		}
-
-
-			if ((selectedEnemyHealthPoints - selectedCharacterAttackPower) > 0) {
-	 			selectedEnemyHealthPoints = selectedEnemyHealthPoints - selectedCharacterAttackPower;
-	 			$("#fight-section #selected-enemy .ui-widget-content .characterHealth").html(selectedEnemyHealthPoints);
-	 			var updatedCharacterAttackPower = selectedCharacterAttackPower + powerSurge;
-	 			console.log("Power surge is working: " + updatedCharacterAttackPower);
-	 			$("#selected-character #character-placeholder .ui-widget-content").attr("data-attackPower", updatedCharacterAttackPower);
-	 			enemyDefeated = false;
-	 		} else if ((selectedEnemyHealthPoints - selectedCharacterAttackPower) <= 0) {
-	 			selectedEnemyHealthPoints = selectedEnemyHealthPoints - selectedCharacterAttackPower;
-	 			$("#fight-section #selected-enemy .ui-widget-content .characterHealth").html(selectedEnemyHealthPoints);
-
-	 			//selectedEnemy.attr("data-isEnemy", "false");
-	 			// Todo: remove the enemy you beat.
-	 			var currentEnemy = $("#fight-section #selected-enemy .ui-widget-content");
-				currentEnemy.css('visibility', 'hidden');
-
-				enemyDefeated = true;
-				enemySelected = false;
-
-				$("#losses-report").empty();
-
-	 			if ($(".enemyList").is(":empty")) {
-	 				var enemyDefeatedMessage = "You Won!!! GAME OVER!!!";
-	 				$("#attack-report").text(enemyDefeatedMessage);
-	 				var restartBtn = $("<button>");
-	 				restartBtn.text("Restart");
-	 				restartBtn.addClass("restart");
-	 				restartBtn.appendTo("#fight-section");
-	 				gameOn = false;
-	 			} else {
-	 				var enemyDefeatedMessage = "You have defeated " + selectedEnemy + ", " + "you can choose to fight another enemy.";
-	 				$("#attack-report").text(enemyDefeatedMessage);
-	 			}
-	 				 			
-	 			// Todo: at any point you get to zero, you should die.
-	 			// Todo: once you beat the last creature, you will see the you won message, and the restart button will reappear.
-	 			// Todo: use awesome trick of descendant selectors in inspector to get colors in boxes, etc.
-	 			// Todo: one tricky bit might be the enemySelected boolean, so if anything is annoying it is probably that.
-
-			} else {
-	 			// Todo: if you press attack button, message will change again reminding you there is no enemy.
-	 			var noEnemyMessage = "No enemy here.";
-	 			$("#attack-report").text(noEnemyMessage);
-	 		}
-		} else {
-			console.log("Attack button shouldn't work when game is over.");
-		}	
-    });
+		 			// Process fallen enemy.
+		 			enemyLost(selectedEnemyHealthPoints, selectedCharacterAttackPower, selectedEnemy);
+				} else {
+				console.log("Attack button shouldn't work when game is over.");
+				}
+	    	}
+	  	});
+	}
 	
-	//$("#fight-section").on("click", ".restart", createCharacterList);
+	// Restarts game on restart click.
 	$("#fight-section").on("click", ".restart", function() {
 		$("ol").addClass("characterList").removeClass("enemyList");
 		$(".characterList").appendTo($("#available-characters"));
@@ -258,4 +240,82 @@ $(document).ready(function() {
 		characterSelected = false;
 		enemySelected = false;
     });
+
+	// Handles attack.
+    function characterAttacks(selectedCharacterHealthPoints, selectedEnemyCounterAttackPoints, selectedCharacterAttackPower, selectedEnemy) {
+    	
+    	// Adjusts character score.
+    	selectedCharacterHealthPoints = selectedCharacterHealthPoints - selectedEnemyCounterAttackPoints;
+		$("#selected-character #character-placeholder .ui-widget-content .characterHealth").html(selectedCharacterHealthPoints);
+		
+		// Creates report.
+		var characterReport = "You attacked " + selectedEnemy + " for " + selectedCharacterAttackPower + " damage.";
+		var enemyReport = selectedEnemy + " attacked you back for " + selectedEnemyCounterAttackPoints + " damage.";
+		$("#attack-report").text(characterReport);
+		$("#losses-report").text(enemyReport);
+    };
+
+    // Handles character loss and end of game.
+    function characterLost(selectedCharacterHealthPoints, selectedEnemyCounterAttackPoints) {
+    	var gameLostMessage = "You've been defeated . . . GAME OVER!!!";
+    	selectedCharacterHealthPoints = selectedCharacterHealthPoints - selectedEnemyCounterAttackPoints;
+    	$("#selected-character #character-placeholder .ui-widget-content .characterHealth").html(selectedCharacterHealthPoints);
+    	$("#attack-report").text(gameLostMessage);
+    	$("#losses-report").empty();
+    	
+    	// Adds restart button.
+    	var restartBtn = $("<button>");
+    	restartBtn.text("Restart");
+    	restartBtn.addClass("restart");
+    	restartBtn.appendTo("#fight-section");
+    };
+
+    // Handles enemy counter attack.
+    function enemyAttacksBack(selectedEnemyHealthPoints, selectedCharacterAttackPower) {
+    	selectedEnemyHealthPoints = selectedEnemyHealthPoints - selectedCharacterAttackPower;
+    	$("#fight-section #selected-enemy .ui-widget-content .characterHealth").html(selectedEnemyHealthPoints);
+    	var updatedCharacterAttackPower = selectedCharacterAttackPower + powerSurge;
+    	console.log("Power surge is working: " + updatedCharacterAttackPower);
+    	$("#selected-character #character-placeholder .ui-widget-content").attr("data-attackPower", updatedCharacterAttackPower);
+    	enemyDefeated = false;
+    }
+
+    // Handles enemy loss.
+	function enemyLost(selectedEnemyHealthPoints, selectedCharacterAttackPower, selectedEnemy) {
+		selectedEnemyHealthPoints = selectedEnemyHealthPoints - selectedCharacterAttackPower;
+		$("#fight-section #selected-enemy .ui-widget-content .characterHealth").html(selectedEnemyHealthPoints);
+
+		var currentEnemy = $("#fight-section #selected-enemy .ui-widget-content");
+		currentEnemy.css('visibility', 'hidden');
+
+		enemyDefeated = true;
+		enemySelected = false;
+
+		$("#losses-report").empty();
+
+		// Game is won when all enemies have been defeated.
+		if ($(".enemyList").is(":empty")) {
+			var enemyDefeatedMessage = "You Won!!! GAME OVER!!!";
+			$("#attack-report").text(enemyDefeatedMessage);
+			
+			// Adds restart button.
+			var restartBtn = $("<button>");
+			restartBtn.text("Restart");
+			restartBtn.addClass("restart");
+			restartBtn.appendTo("#fight-section");
+			gameOn = false;
+
+		// Controls ability to select new enemy.
+		} else {
+			var enemyDefeatedMessage = "You have defeated " + selectedEnemy + ", " + "you can choose to fight another enemy.";
+			$("#attack-report").text(enemyDefeatedMessage);
+
+			// I tried hard to disable the attack button after enemy loses, but couldn't.
+			//$("#attack").attr("disabled", true);
+		}
+		//} else if ($("#attack").attr("disabled", true)){
+			// Todo: if you press attack button, message will change again reminding you there is no enemy.
+			//var noEnemyMessage = "No enemy here.";
+			//$("#attack-report").text(noEnemyMessage);
+    };
 });
